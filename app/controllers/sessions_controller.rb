61f509f75447a4f361e305ab66967c306baa16ad
Your name <Your email>
2019-06-10 10:14:55 -0700

    Rrestrict access to the root to logged in users only. Restrict access to login page to non siged in users only.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,5 @@
 class SessionsController < ApplicationController
-  # before_action :set_session, only: [:show, :edit, :update, :destroy]
+  before_action :logged_in_redirect, only: [:new, :create]
 
   # GET /sessions
   # GET /sessions.json
@@ -7,18 +7,9 @@ class SessionsController < ApplicationController
     @sessions = Session.all
   end
 
-  # GET /sessions/1
-  # GET /sessions/1.json
-  def show
-  end
-
   # GET /sessions/new
   def new
 
-  end
-
-  # GET /sessions/1/edit
-  def edit
   end
 
   # POST /sessions
@@ -37,22 +28,6 @@ class SessionsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /sessions/1
-  # PATCH/PUT /sessions/1.json
-  # def update
-  #   respond_to do |format|
-  #     if @session.update(session_params)
-  #       format.html { redirect_to @session, notice: 'Session was successfully updated.' }
-  #       format.json { render :show, status: :ok, location: @session }
-  #     else
-  #       format.html { render :edit }
-  #       format.json { render json: @session.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
-  #
-  # DELETE /sessions/1
-  # DELETE /sessions/1.json
   def destroy
     session[:user_id] = nil
     flash[:success] = "You have successfully logged out"
@@ -60,13 +35,11 @@ class SessionsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_session
-      @session = Session.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def session_params
-      params.fetch(:session, {})
+  def logged_in_redirect
+    if logged_in?
+      flash[:error] = "You are already logged in"
+      redirect_to root_path
     end
+  end
 end

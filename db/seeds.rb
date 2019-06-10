38dfb35cb17a1aaf255d81efc917f6dcda38d2b3
Your name <Your email>
2019-06-10 13:54:49 -0700

    uncomment seed file so we can create some users

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# User.create(username: "Allan", email: "allan@example.com", password: "123qweasdzxc")
-# User.create(username: "Brian", email: "brian@example.com", password: "qwert12345")
-# User.create(username: "Charles", email: "chuck@example.com", password: "typewriter")
-# User.create(username: "Dianne", email: "dianne@example.com", password: "zmxncbv")
-# User.create(username: "Elaine", email: "elaine@example.com", password: "laksjdhfg")
+User.create(username: "Allan", email: "allan@example.com", password: "123qweasdzxc")
+User.create(username: "Brian", email: "brian@example.com", password: "qwert12345")
+User.create(username: "Charles", email: "chuck@example.com", password: "typewriter")
+User.create(username: "Dianne", email: "dianne@example.com", password: "zmxncbv")
+User.create(username: "Elaine", email: "elaine@example.com", password: "laksjdhfg")
 
 # User.all.each do |user|
 #   Message.create(body: "#{Faker::Lorem.sentence(3, true)}", user_id: user.id)
